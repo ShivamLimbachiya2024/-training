f1e6b84dcd9a508184853656482f7968ad5b63ef
ShivamLimbachiya2024 <shivam.limbachiya.2024@gmail.com>
2024-03-22 15:15:50 +0530

    Added Validations

--- public/script.js
+++ public/script.js
@@ -42,12 +42,62 @@ const validation = () => {
     var dob = document.getElementById('dob')
     var email = document.getElementById('email')
     var phone = document.getElementById('phone')
-    var stateselect = document.getElementById('stateselec')
+    var stateselect = document.getElementById('stateselect')
     var cityselect = document.getElementById('cityselect')
     if (document.getElementById("M").checked == true || document.getElementById("F").checked == true) {
         // isValid = true;
+        document.getElementById('gendermsg').innerHTML=''
     } else {
         isValid = false;
+        document.getElementById('gendermsg').innerHTML='PLZ SELECT GENDER!!'
+    }
+    if (regName.test(fname.value)==false) {
+        fname.nextElementSibling.innerHTML="Invalid fname"
+        isValid=false
+    }else{
+        fname.nextElementSibling.innerHTML=''
+    }
+    if (regName.test(lname.value)==false) {
+        lname.nextElementSibling.innerHTML="Invalid lname"
+        isValid=false
+    }else{
+        lname.nextElementSibling.innerHTML=''
+    }
+    if (dateReg.test(dob.value)==false) {
+        dob.nextElementSibling.innerHTML="Invalid dob"
+        isValid=false
+    }else{
+        dob.nextElementSibling.innerHTML=''
+    }
+    if (emailReg.test(email.value)==false) {
+        email.nextElementSibling.innerHTML="Invalid email"
+        isValid=false
+    }else{
+        email.nextElementSibling.innerHTML=''
+    }
+    if (phoneReg.test(phone.value)==false) {
+        phone.nextElementSibling.innerHTML="Invalid phone"
+        isValid=false
+    }else{
+        phone.nextElementSibling.innerHTML=''
+    }
+    if (phoneReg.test(phone.value)==false) {
+        phone.nextElementSibling.innerHTML="Invalid phone"
+        isValid=false
+    }else{
+        phone.nextElementSibling.innerHTML=''
+    }
+    if (stateselect.value=='') {
+        stateselect.nextElementSibling.innerHTML='Plz Select'
+        isValid=false
+    }else{
+        stateselect.nextElementSibling.innerHTML=''        
+    }
+    if (cityselect.value=='') {
+        cityselect.nextElementSibling.innerHTML='Plz Select'
+        isValid=false
+    }else{
+        cityselect.nextElementSibling.innerHTML=''        
     }
     return isValid;
 }
