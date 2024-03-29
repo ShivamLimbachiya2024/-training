212a13582c58973bdf8144f18cbb1e0800241016
ShivamLimbachiya2024 <shivam.limbachiya.2024@gmail.com>
2024-03-29 14:38:01 +0530

    Enhansed UI for better user experience

--- controller/SpecificSearchquery.js
+++ controller/SpecificSearchquery.js
@@ -1,30 +1,15 @@
 const con = require("../modules/connection");
 const studentlist = (req, res) => {
     var studentid = Number(req.body.studentid);
-    var { fname, lname, email, phoneno, city, state } = req.body;
-    var orderby = req.query.orderby;
-    var sortby = req.query.sortby;
+    var { fname, lname, email, phoneno, city } = req.body;
     let recordsinonepage = 200;
     var pagenumber = req.query.pageid;
-
-    if (
-        orderby == undefined ||
-        orderby == null ||
-        sortby == undefined ||
-        sortby == null
-    ) {
-        orderby = "StudentID";
-        sortby = "asc";
-    }
-
     if (
         req.query.fname ||
-        req.query.state ||
         req.query.lname ||
         req.query.city
     ) {
         fname = req.query.fname;
-        state = req.query.state;
         lname = req.query.lname;
         city = req.query.city;
     }
@@ -40,7 +25,7 @@ const studentlist = (req, res) => {
     }
 
     var select = `select sid As StudentID,fname As FirstName ,lname As LastName, email As Email,phone As MobileNumber,age as Age, gender As Gender, city As City,zipcode As PinCode from Student_Master_feb26 limit ${start},${recordsinonepage}`;
-    if (studentid || fname || lname || email || phoneno || city || state) {
+    if (studentid || fname || lname || email || phoneno || city) {
         select = `select sid As StudentID, fname As FirstName, lname As LastName, email As Email, phone As MobileNumber,age as Age, gender As Gender, city As City, zipcode As PinCode from Student_Master_feb26 where sid like '${studentid}' or fname like '${fname}' or email like '${email}' or lname like '${lname}' or phone like '${phoneno}' 
                 or city like '${city}' limit ${start},${recordsinonepage}` ;
     }
@@ -53,10 +38,7 @@ const studentlist = (req, res) => {
                 row: row,
                 col: col,
                 pageid: pagenumber,
-                orderby: orderby,
-                sortby: sortby,
                 fname: fname,
-                state: state,
                 city: city,
                 lname: lname,
             });
