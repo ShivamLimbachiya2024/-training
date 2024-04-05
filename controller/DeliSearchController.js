8e3a2e35fd1794d13e72cad8bf4a781290794f6a
ShivamLimbachiya2024 <shivam.limbachiya.2024@gmail.com>
2024-04-05 17:10:00 +0530

    Improved file format

--- controller/DeliSearchController.js
+++ controller/DeliSearchController.js
@@ -12,27 +12,12 @@ const runQuery = (req, res) => {
   let sql = "";
   if (req.body.input) {
     for (let i = 0; i <= body.length; i++) {
-      if (
-        body.charAt(i) == "_" ||
-        body.charAt(i) == "$" ||
-        body.charAt(i) == "{" ||
-        body.charAt(i) == "}" ||
-        body.charAt(i) == "/"
-      ) {
+      if (body.charAt(i) == "_" || body.charAt(i) == "$" || body.charAt(i) == "{" || body.charAt(i) == "}" || body.charAt(i) == "/") {
         currentDel = i;
-
         for (let j = i + 1; j <= body.length; j++) {
-          if (
-            body.charAt(j) == "_" ||
-            body.charAt(j) == "$" ||
-            body.charAt(j) == "{" ||
-            body.charAt(j) == "}" ||
-            body.charAt(j) == "/"
-          ) {
+          if (body.charAt(j) == "_" || body.charAt(j) == "$" || body.charAt(j) == "{" || body.charAt(j) == "}" || body.charAt(j) == "/") {
             nextDel = j;
-
             const del = body.charAt(currentDel);
-
             switch (del) {
               case "_":
                 fname.push(body.slice(currentDel + 1, nextDel));
@@ -54,39 +39,12 @@ const runQuery = (req, res) => {
         }
       }
     }
-
-    function stringForm(arr, field) {
-      let tempStr = "";
-      arr.forEach((element) => {
-        if (element == undefined) {
-          tempStr = tempStr + field + " like '%' or ";
-        } else {
-          tempStr = tempStr + field + " like '" + element + "%' or ";
-        }
-      });
-
-      return tempStr.slice(0, -4);
-    }
-
     const fname_str = stringForm(fname, "fname");
     const lname_str = stringForm(lname, "lname");
     const email_str = stringForm(email, "email");
     const city_str = stringForm(city, "city");
     let queryholder = [fname_str, lname_str, email_str, city_str];
     let query = queryGen(queryholder);
-
-    function queryGen(queryholder) {
-      let random = "";
-      queryholder.forEach((element) => {
-        if (element == "") {
-          random = random + "";
-        } else {
-          random = random + "(" + element + ") and";
-        }
-      });
-      return random.slice(0, -4);
-    }
-
 
     sql = `select fname as First_Name,lname as Last_Name,email as Email,city as City from  Student_Master_feb26 where ${query};`;
   } else {
@@ -112,4 +70,27 @@ const runQuery = (req, res) => {
     }
   });
 };
+function stringForm(arr, field) {
+  let tempStr = "";
+  arr.forEach((element) => {
+    if (element == undefined) {
+      tempStr = tempStr + field + " like '%' or ";
+    } else {
+      tempStr = tempStr + field + " like '" + element + "%' or ";
+    }
+  });
+
+  return tempStr.slice(0, -4);
+}
+function queryGen(queryholder) {
+  let random = "";
+  queryholder.forEach((element) => {
+    if (element == "") {
+      random = random + "";
+    } else {
+      random = random + "(" + element + ") and";
+    }
+  });
+  return random.slice(0, -4);
+}
 module.exports = { runQuery }
